76e315fdad584ea39b53fad6eb2d0a85731bb17b
BharathKram <78423364+BharathKram@users.noreply.github.com>
2021-02-12 08:41:11 +0530

    Add files via upload

---
+++ exampledate.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class exampledate {
+    public static void main(String arg[])
+    {
+        Date obj= new Date();
+        //int da=29-10-1997;
+        System.out.println("the day is"+obj.getDay();
+    }
+}
+
